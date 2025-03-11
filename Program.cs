fbff38e97b29ca74799398fc155b317d9f4c3210
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-03-11 08:39:39 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+abstract class Character
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int AttackPower { get; set; }
+
+    public Character(string name, int health, int attackPower)
+    {
+        Name = name;
+        Health = health;
+        AttackPower = attackPower;
+    }
+
+    public abstract void Attack(Character target);
+}
+
+class Archer : Character
+{
+    public Archer(string name) : base(name, 80, 15) { }
+
+    public override void Attack(Character target)
+    {
+        Console.WriteLine($"{Name} bắn tên vào {target.Name} gây {AttackPower} sát thương!");
+        target.Health -= AttackPower;
+    }
+}
+
+class Warrior : Character
+{
+    public Warrior(string name) : base(name, 120, 10) { }
+
+    public override void Attack(Character target)
+    {
+        Console.WriteLine($"{Name} chém {target.Name} gây {AttackPower} sát thương!");
+        target.Health -= AttackPower;
+    }
+}
+
+class Mage : Character
+{
+    public Mage(string name) : base(name, 70, 20) { }
+
+    public override void Attack(Character target)
+    {
+        Console.WriteLine($"{Name} dùng phép thuật tấn công {target.Name} gây {AttackPower} sát thương!");
+        target.Health -= AttackPower;
+    }
+}
+
+class Game
+{
+    static void Main()
+    {
+        Archer archer = new Archer("Legolas");
+        Warrior warrior = new Warrior("Thorin");
+        Mage mage = new Mage("Gandalf");
+
+        archer.Attack(warrior);
+        warrior.Attack(mage);
+        mage.Attack(archer);
+
+        Console.WriteLine($"{warrior.Name} còn {warrior.Health} HP");
+        Console.WriteLine($"{mage.Name} còn {mage.Health} HP");
+        Console.WriteLine($"{archer.Name} còn {archer.Health} HP");
+    }
+}
